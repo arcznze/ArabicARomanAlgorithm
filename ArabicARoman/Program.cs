c46570101930a30e3369552088d6efbb87a063de
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-05-17 03:02:32 -0400

    Validation ready.

--- ArabicARoman/Program.cs
+++ ArabicARoman/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+string input;
+Console.WriteLine("Enter a number: ");
+input = Console.ReadLine();
+
+Regex regex = new Regex(@"^\d+$");
+if (regex.IsMatch(input))
+{
+    Check();
+}
+else
+    Console.WriteLine("You can only write numbers.");
+
+void Check()
+{
+    if (int.Parse(input) <= 0)
+        Console.WriteLine("You can only write numbers greater than zero.");
+    else if (int.Parse(input) >= 1000000)
+        Console.WriteLine("You cannot write numbers greater than a million.");
+    else
+        Console.WriteLine("Valid");
+}
