cc0f5595a1140c9e3f8d95019abe18bf63374fda
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-05-17 03:35:06 -0400

    Ten, Hundred and Thousand can now convert into Romans.

--- ArabicARoman/Program.cs
+++ ArabicARoman/Program.cs
@@ -4,6 +4,8 @@ string input;
 string[] x = new string[6];
 string[] unit = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 string[] ten = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+string[] hundred = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+string[] thousand = { "", "M", "MM", "MMM", "|IV|", "|V|", "|V|M", "|V|MM", "|V|MMM", "|IX|" };
 Console.WriteLine("Enter a number: ");
 input = Console.ReadLine();
 
@@ -40,7 +42,35 @@ void GetRomans()
         string b = unit[a];
         Console.WriteLine(b);
     }
-    else
-        Console.WriteLine("");
+    else if (input.Length == 2)
+    {
+        int c = int.Parse(x[0]);
+        string d = ten[c];
+        int a = int.Parse(x[1]);
+        string b = unit[a];
+        Console.WriteLine(d + b);
+    }
+    else if(input.Length == 3)
+    {
+        int e = int.Parse(x[0]);
+        string f = hundred[e];
+        int c = int.Parse(x[1]);
+        string d = ten[c];
+        int a = int.Parse(x[2]);
+        string b = unit[a];
+        Console.WriteLine(f + d + b);
+    }
+    else if(input.Length == 4)
+    {
+        int g = int.Parse(x[0]);
+        string h = thousand[g];
+        int e = int.Parse(x[1]);
+        string f = hundred[e];
+        int c = int.Parse(x[2]);
+        string d = ten[c];
+        int a = int.Parse(x[3]);
+        string b = unit[a];
+        Console.WriteLine(h + f + d + b);
+    }
     
 }
